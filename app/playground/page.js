e32bd2ff28f291d67baf1bf66fa596aac5b30e45
iovel-mer <ioveeel.mind@gmail.com>
2025-03-18 15:16:31 +0400

    Launched

---
+++ app/playground/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='space-y-8'>
+        <h1 className="text-4xl mt-8"></h1>
+
+        <div>
+            <h2 className="mb-4 text-lg font-mon">CompName</h2>
+            <h1 className="mb-4 border-gray-200 dark:border-gray-800"></h1>
+            <div>Here</div>
+        </div>
+    </main>
+  )
+}
+
+export default page
