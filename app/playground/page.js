cf271cae3748f5fc68b5581c5623f8e96f021c67
iovel-mer <ioveeel.mind@gmail.com>
2025-03-19 12:58:33 +0400

    added transactions

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,19 +1,19 @@
-import Pageheader from '@/components/page-header'
 import Trend from '@/components/trend'
-import React from 'react'
-
-const page = () => {
-  return (
-    <main className='space-y-8'>
-        <h1 className="text-4xl mt-8">Playground</h1>
-
-        <div>
-            <h2 className="mb-4 text-lg font-mon">PageHeader</h2>
-            <h1 className="mb-4 border-gray-200 dark:border-gray-800"></h1>
-            <div><Pageheader/></div>
-        </div>
-
-        <div>
+ import PageHeader from '@/components/page-header'
+ import TransactionItem from '@/components/transaction-item';
+ 
+ export default function Page() {
+   return (
+     <main className="space-y-8">
+       <h1 className="text-4xl mt-8">Playground</h1>
+ 
+       <div>
+         <h2 className="mb-4 text-lg font-mono">PageHeader</h2>
+         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+         <div><PageHeader /></div>
+       </div>
+ 
+       <div>
          <h2 className="mb-4 text-lg font-mono">Trend</h2>
          <hr className="mb-4 border-gray-200 dark:border-gray-800" />
          <div className="flex space-x-8">
@@ -23,8 +23,17 @@ const page = () => {
            <Trend type="Saving" amount={500} prevAmount={950} />
          </div>
        </div>
-    </main>
-  )
-}
-
-export default page
+ 
+       <div>
+         <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
+         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+         <div className="space-y-4">
+           <TransactionItem type="Income" description="Salary" amount={2000} />
+           <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+           <TransactionItem type="Saving" description="For children" amount={500} />
+           <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
+         </div>
+       </div>
+     </main>
+   )
+ }
