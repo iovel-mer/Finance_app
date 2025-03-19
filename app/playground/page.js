62293d5ec7d31835066f7f3ad8ad84c25fbabe70
iovel-mer <ioveeel.mind@gmail.com>
2025-03-19 17:44:20 +0400

    components label,input,select,button

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,10 +1,15 @@
 import Trend from '@/components/trend'
  import PageHeader from '@/components/page-header'
  import TransactionItem from '@/components/transaction-item';
+import TransactionSummaryItem from '@/components/transaction-summary';
+import Button from '@/components/button';
+import Label from '@/components/label';
+ import Input from '@/components/input';
+ import Select from '@/components/select';
  
  export default function Page() {
    return (
-     <main className="space-y-8">
+     <main className="space-y-8 mb-44">
        <h1 className="text-4xl mt-8">Playground</h1>
  
        <div>
@@ -32,6 +37,58 @@ import Trend from '@/components/trend'
            <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
            <TransactionItem type="Saving" description="For children" amount={500} />
            <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
+         </div>
+       </div>
+
+
+       <div>
+         <h2 className="mb-4 text-lg font-mono">TransactionSummaryItem + TransactionItem</h2>
+         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+         <div className="space-y-4">
+           <TransactionSummaryItem date="2024-05-01" amount={3500} />
+           <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+           <TransactionItem type="Income" description="Salary" amount={2000} />
+           <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+           <TransactionItem type="Saving" description="For children" amount={500} />
+           <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
+         </div>
+       </div>
+       <div>
+         <h2 className="mb-4 text-lg font-mono">Buttons</h2>
+         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+         <div className="space-x-4">
+           <Button>Hello</Button>
+           <Button variant="outline">Hello</Button>
+           <Button variant="ghost">Hello</Button>
+ 
+           <Button size="xs">Hello</Button>
+           <Button size="sm">Hello</Button>
+           <Button size="lg">Hello</Button>
+         </div>
+       </div>
+
+       <div>
+         <h2 className="mb-4 text-lg font-mono">Forms</h2>
+         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+         <div className="grid grid-cols-2 gap-4">
+           <div>
+           <Label className="mb-1">Your name</Label>
+           <Input type="text" placeholder="Type something in here..." />
+           </div>
+ 
+           <div>
+           <Label className="mb-1">City</Label>
+           <Select>
+               <option>Warsaw</option>
+               <option>Berlin</option>
+               <option>London</option>
+               </Select>
+           </div>
+ 
+           <div className="flex items-center">
+           <Input type="checkbox" id="terms" />
+           <Label className="ml-2" htmlFor="terms">Accept terms</Label>
+           </div>
          </div>
        </div>
      </main>
